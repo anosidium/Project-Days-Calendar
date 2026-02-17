de04ac94bc5dc7727944ed4f7db4e3e6dab86b83
Ammad <anosidium@hotmail.com>
2026-02-17 22:27:22 +0000

    Wire up buttons to function

--- web.mjs
+++ web.mjs
@@ -6,4 +6,22 @@
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-window.addEventListener("load", () => {});
+function previousMonth() {}
+
+function nextMonth() {}
+
+function changeMonth() {}
+
+function changeYear() {}
+
+window.addEventListener("load", () => {
+  const previousMonthButton = document.getElementById("prev-month");
+  const nextMonthButton = document.getElementById("next-month");
+  const monthSelectControl = document.getElementById("month-select");
+  const yearSelectControl = document.getElementById("year-select");
+
+  previousMonthButton.addEventListener("click", previousMonth);
+  nextMonthButton.addEventListener("click", nextMonth);
+  monthSelectControl.addEventListener("change", changeMonth);
+  yearSelectControl.addEventListener("change", changeYear);
+});
