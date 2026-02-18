025ad3b53dd583d57bf1b5737d5a123c66ef5883
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-02-18 16:35:52 +0000

    test: add tests for getDateForDay function

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,40 @@
-import { getGreeting } from "./common.mjs";
+import { getDateForDay } from "./common.mjs";
 import assert from "node:assert";
-import test from "node:test";
+import { describe, test } from "node:test";
 
-test("Greeting is correct", () => {
-  assert.equal(getGreeting(), "Hello");
-});
+(describe("getDateForDay returns the correct date for a commemorative day"),
+  () => {
+    test("Ada Lovelace Day 2024 is October 8", () => {
+      const day = {
+        monthName: "October",
+        dayName: "Tuesday",
+        occurrence: "second",
+      };
+      const result = getDateForDay(day, 2024);
+      assert.strictEqual(result.getDate(), 8);
+      assert.strictEqual(result.getMonth(), 9);
+      assert.strictEqual(result.getFullYear(), 2024);
+    });
+
+    test("Ada Lovelace Day 2020 is October 13", () => {
+      const day = {
+        monthName: "October",
+        dayName: "Tuesday",
+        occurrence: "second",
+      };
+      const result = getDateForDay(day, 2020);
+      assert.strictEqual(result.getDate(), 13);
+      assert.strictEqual(result.getMonth(), 9);
+    });
+
+    test("Ada Lovelace Day 2025 is October 14", () => {
+      const day = {
+        monthName: "October",
+        dayName: "Tuesday",
+        occurrence: "second",
+      };
+      const result = getDateForDay(day, 2025);
+      assert.strictEqual(result.getDate(), 14);
+      assert.strictEqual(result.getMonth(), 9);
+    });
+  });
