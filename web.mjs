30e5927726b30e4f31d928293e9230ca191afa2a
Ammad <anosidium@hotmail.com>
2026-02-18 14:00:43 +0000

    Delete unused code

--- web.mjs
+++ web.mjs
@@ -3,9 +3,6 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getGreeting } from "./common.mjs";
-import daysData from "./days.json" with { type: "json" };
-
 const currentMonthYear = document.getElementById("current-month-year");
 const previousMonthButton = document.getElementById("prev-month");
 const nextMonthButton = document.getElementById("next-month");
