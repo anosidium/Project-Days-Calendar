7b1579cc0c286d2fed96b91d5901885bf0255956
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-02-18 14:33:55 +0000

    style: add Prettier configuration and clean up code formatting

--- web.mjs
+++ web.mjs
@@ -1,8 +1,3 @@
-// This is a placeholder file which shows how you can access functions and data defined in other files.
-// It can be loaded into index.html.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
 const currentMonthYear = document.getElementById("current-month-year");
 const previousMonthButton = document.getElementById("prev-month");
 const nextMonthButton = document.getElementById("next-month");
@@ -53,7 +48,10 @@ function populateYearSelectControl(element) {
 }
 
 function formatMonthYear(month, year) {
-  return new Date(year, month).toLocaleString(undefined, { month: "long", year: "numeric" });
+  return new Date(year, month).toLocaleString(undefined, {
+    month: "long",
+    year: "numeric",
+  });
 }
 
 function updateHeader() {
